388e3c54beedd6b8584624f6261b4a8c991a664e
sahiljain112 <sahiljain112@gmail.com>
2016-12-03 00:11:05 +0530

    Env var check 2

--- app.js
+++ app.js
@@ -19,27 +19,27 @@ app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
-
-
-const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
-  process.env.MESSENGER_APP_SECRET :
-  config.get('appSecret');
-
-// Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-  (process.env.MESSENGER_VALIDATION_TOKEN) :
-  config.get('validationToken');
-
-// Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-  config.get('pageAccessToken');
-
-// URL where the app is running (include protocol). Used to point to scripts and
-// assets located at this address.
-const SERVER_URL = (process.env.SERVER_URL) ?
-  (process.env.SERVER_URL) :
-  config.get('serverURL');
+// 
+//
+// const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+//   process.env.MESSENGER_APP_SECRET :
+//   config.get('appSecret');
+//
+// // Arbitrary value used to validate a webhook
+// const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+//   (process.env.MESSENGER_VALIDATION_TOKEN) :
+//   config.get('validationToken');
+//
+// // Generate a page access token for your page from the App Dashboard
+// const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+//   (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+//   config.get('pageAccessToken');
+//
+// // URL where the app is running (include protocol). Used to point to scripts and
+// // assets located at this address.
+// const SERVER_URL = (process.env.SERVER_URL) ?
+//   (process.env.SERVER_URL) :
+//   config.get('serverURL');
 //
 // const APP_SECRET = ENV['MESSENGER_APP_SECRET'];
 // const VALIDATION_TOKEN = ENV['VALIDATION_TOKEN'];
