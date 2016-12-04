2fddec66ddbc67a66eb38cec58aeae6aee4fdf4d
sahiljain112 <sahiljain112@gmail.com>
2016-12-05 04:27:09 +0530

    Mapping and queries added

---
+++ mapping.js
@@ -0,0 +1,11 @@
+
+const mapping = [
+  { 1: 'Mumbai'} ,
+  { 2: 'Cochin'} ,
+  { 3: 'Chennai'} ,
+  { 4: 'Pune'} ,
+  { 5: 'Calcutta'},
+  { 6: 'Kathmandu'}
+]
+
+module.exports = mapping;
--- query.js
+++ query.js
@@ -1,8 +1,9 @@
 var jsonData = require('./data.json');
+console.log('Data arrived successfully',jsonData[0]);
 
 const getFlight = (date, from, to, limit) => {
 
-  String timeCompare = '00:00:00.000-08:00';
+  let timeCompare = '00:00:00.000-08:00';    // Will not work for midnight flight cases
   date = '' + date;
   const formattedDate = (date).split("T");
   let availableFlights = [];
@@ -27,8 +28,9 @@ const getFlight = (date, from, to, limit) => {
            }
         }
       }
-
   }
+
+  return availableFlights;
 
 }
 
@@ -46,14 +48,74 @@ const findSlot = (date) => {
     return 'N';
 };
 
-const getNearbyFlights = (date, from ,to, limit) => {
+const getBestRecommedations = (date, from, to, limit, suggestionLimit) => {
+
+  let formattedDate = ('' + date).split("T");
+  let timeCompare = '00:00:00.000-08:00';
+  date = formattedDate[0] + timeCompare;
+
+  let availableFlights = getBestFlight(date, from, to, limit);
+  let bestFlights = getBestFlights(availableFlights, suggestionLimit);
 
   let goodNearbyFlights = [];
+  let tomorrow = date, previous = date;
+
   for(let i = 0; i < limit; i++){
-      let tomorrow = new Date().setDate(date.getDate()+1);
-      goodNearbyFlights = goodNearbyFlights + getFlight( ,from, to, limit);
+      tomorrow = new Date(tomorrow.getTime() + 1000*60*60*24 );
+      goodNearbyFlights = goodNearbyFlights.concat(getFlight(tomorrow ,from, to, limit));
   }
-  
+
+  const timeDiff = Math.abs(previous.getTime() - tomorrow.getTime());
+  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+  if(diffDays < limit)
+    limit = diffDays;
+
+    for(let i = 0; i < limit; i++){
+        previous = new Date(previous.getTime() - 1000*60*60*24 );
+        goodNearbyFlights = goodNearbyFlights.concat(getFlight( previous ,from, to, limit));
+    }
+
+  bestFlights = bestFlights.concat(getBestFlights(goodNearbyFlights, suggestionLimit));
+  bestFlights = bestFlights.slice(1);
+  return bestFlights;
+
+};
+
+const getBestFlights = (flights, limit) => {
+
+  flights.sort((a,b) => {
+      return a.cost - b.cost;
+  });
+
+  return flights.slice(limit);
+};
+
+const checkSeatAvailablity = (date) => {
+  const timeDiff = Math.abs(previous.getTime() - tomorrow.getTime());
+  const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+    const seatsLeft = predictSeatsLeft(diffDays, Math.floor(Math.random() * 4));
+
+    if(seatsLeft <= 5)
+      finalValue = 'Filling Fast';
+    else if(seatsLeft <= 10)
+      finalValue = 'Filling Normally';
+    else
+      finalValue = 'Filling Safely';
+
+    return [ seatsLeft, finalValue];
+};
+
+// Predicts the number of seats left based on a quadratic function. Assuming initially there are a 100 seats left!
+const predictSeatsLeft = (x, r) => {
+
+      let y = r - 100/r
+      y = y.toFixed(2);
+
+      var f = (x-r)*((x-r) + y);
+      return f;
+
 };
 
 module.exports = {
